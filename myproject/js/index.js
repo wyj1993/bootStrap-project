edce272a2d97b86b1c74a32ca8008eb11677f9ac
wyj1993 <1959617212@qq.com>
2019-01-18 23:22:01 +0800

    导航栏子菜单

--- myproject/js/index.js
+++ myproject/js/index.js
@@ -1,6 +1,7 @@
 
     (function(){
-        // 轮播图
+        /****banner */
+        /*
     function banner(){
     var li=document.querySelector(".banner>.banner_left>ul:first-child>li.open");
     li.className="";
@@ -17,5 +18,36 @@
     }
     bannerUl.onmouseout=function(){
         n=setInterval(banner,3000);
-    }
+    }*/
+    /*****  sub-nav *** */
+    $(".sub-nav").children().hide();
+    $("[data-toggle=nav]").mouseenter(
+        function(){
+           var id= $(this).attr("data-target");
+           $(id).css("zIndex",10).show();
+        }
+    )
+    .mouseleave(
+        function(){
+            var $a=$(this);
+            function task(){
+                var id= $a.attr("data-target"); 
+                if(!$(id).is(':hover')) $(id).hide()
+            }
+            var n=setTimeout(task,100)
+        }
+    )
+    $(".sub-nav").mouseleave(
+        function (){
+            $(this).children().hide();
+        }
+    )
+
+
+
+
+
+
+
+
 })()
